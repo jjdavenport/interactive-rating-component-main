a38867b2dd274269c5ddb6328875ac9238aaa516
jjdavenport <jordanjamesdavenport@gmail.com>
2024-04-20 15:51:35 +0100

    completed function

--- main.js
+++ main.js
@@ -8,14 +8,77 @@ const btn4 = document.getElementById("btn4");
 const btn5 = document.getElementById("btn5");
 const selected = document.getElementById("selected");
 
-submitBtn.addEventListener("click", () => {
-  display.classList.remove("display");
-  display.classList.add("submit-display");
-  hidden.classList.add("display-hidden");
-  selected.innerText = "You selected 1 out of 5";
-});
-
 btn1.addEventListener("click", () => {
   btn1.classList.add("select-button");
   btn1.classList.remove("button");
+  btn2.classList.add("button");
+  btn3.classList.add("button");
+  btn4.classList.add("button");
+  btn5.classList.add("button");
+  submitBtn.addEventListener("click", () => {
+    display.classList.remove("display");
+    display.classList.add("submit-display");
+    hidden.classList.add("display-hidden");
+    selected.innerText = "You selected 1 out of 5";
+  });
+});
+
+btn2.addEventListener("click", () => {
+  btn2.classList.add("select-button");
+  btn2.classList.remove("button");
+  btn1.classList.add("button");
+  btn3.classList.add("button");
+  btn4.classList.add("button");
+  btn5.classList.add("button");
+  submitBtn.addEventListener("click", () => {
+    display.classList.remove("display");
+    display.classList.add("submit-display");
+    hidden.classList.add("display-hidden");
+    selected.innerText = "You selected 2 out of 5";
+  });
+});
+
+btn3.addEventListener("click", () => {
+  btn3.classList.add("select-button");
+  btn3.classList.remove("button");
+  btn1.classList.add("button");
+  btn2.classList.add("button");
+  btn4.classList.add("button");
+  btn5.classList.add("button");
+  submitBtn.addEventListener("click", () => {
+    display.classList.remove("display");
+    display.classList.add("submit-display");
+    hidden.classList.add("display-hidden");
+    selected.innerText = "You selected 3 out of 5";
+  });
+});
+
+btn4.addEventListener("click", () => {
+  btn4.classList.add("select-button");
+  btn4.classList.remove("button");
+  btn1.classList.add("button");
+  btn2.classList.add("button");
+  btn3.classList.add("button");
+  btn5.classList.add("button");
+  submitBtn.addEventListener("click", () => {
+    display.classList.remove("display");
+    display.classList.add("submit-display");
+    hidden.classList.add("display-hidden");
+    selected.innerText = "You selected 4 out of 5";
+  });
+});
+
+btn5.addEventListener("click", () => {
+  btn5.classList.add("select-button");
+  btn5.classList.remove("button");
+  btn1.classList.add("button");
+  btn2.classList.add("button");
+  btn3.classList.add("button");
+  btn4.classList.add("button");
+  submitBtn.addEventListener("click", () => {
+    display.classList.remove("display");
+    display.classList.add("submit-display");
+    hidden.classList.add("display-hidden");
+    selected.innerText = "You selected 5 out of 5";
+  });
 });
